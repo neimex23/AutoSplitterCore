ba5b65f045be83f23ae5559befd8ff31e50c46ba
Neimex23 <ezequielmedina23@gmail.com>
2022-12-07 23:07:59 -0300

    ASC_v2.6.0 Fix ER

--- Sources/AutoSplitters/EldenSplitter.cs
+++ Sources/AutoSplitters/EldenSplitter.cs
@@ -78,7 +78,7 @@ namespace AutoSplitterCore
         public bool getEldenStatusProcess(int delay) //Use Delay 0 only for first Starts
         {
             Thread.Sleep(delay);
-            return _StatusElden = elden.Refresh(out Exception exc);
+            return _StatusElden = elden.TryRefresh(out Exception exc);
         }
 
         public void setStatusSplitting(bool status)
