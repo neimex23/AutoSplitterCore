ef19c3b88d68d0418d0bc8f035e35adc3f0a47bb
Neimex23 <ezequielmedina23@gmail.com>
2025-02-16 12:36:50 -0300

    Change Router Access to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "tailwindcss";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/AutoSplitterCore">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
